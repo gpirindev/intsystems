94687b5c34415457e7548c925bb28c4459ddcca4
Gregorian Pirindev <ug15gp@ugsapc4.tcs.cs.ox.ac.uk>
2017-11-11 21:20:19 +0000

    AStar Tree Search on big Romania map gives me trouble

--- src/tour/BFTS_Demo.java
+++ src/tour/BFTS_Demo.java
@@ -1,10 +1,10 @@
 package tour;
 
+import search.AStarFunction;
+import search.BestFirstFrontier;
 import search.BreadthFirstFrontier;
-import search.DepthFirstFrontier;
 import search.GoalTest;
 import search.GraphSearch;
-import search.IterativeDeepeningTreeSearch;
 import search.Node;
 import search.Printing;
 import search.Search;
@@ -12,7 +12,7 @@ import search.TreeSearch;
 
 public class BFTS_Demo {
 	public static void main(String[] args) {
-		System.out.println("This is a demonstration of breadth-first tree search on Romania tour");
+		System.out.println("This is a demonstration of search on Romania tour");
 		System.out.println();
 		
 		Cities romania = SetUpRomania.getRomaniaMap();
@@ -20,36 +20,19 @@ public class BFTS_Demo {
 		
 		GoalTest goalTest = new TourGoalTest(romania.getAllCities(), startCity);
 		
-		Node root = new Node(null, null, new TourState(startCity), 0);
+		Node root = new Node(null, null, new TourState(startCity));
     Printing printing = new TourPrinting();
-    Search BFTS = new TreeSearch(new BreadthFirstFrontier());
-    Search BFGS = new GraphSearch(new BreadthFirstFrontier());
-    Search DFTS = new TreeSearch(new DepthFirstFrontier());
-    Search DFGS = new GraphSearch(new DepthFirstFrontier());
-    Search IDTS = new IterativeDeepeningTreeSearch(1);
-    Node solution = BFTS.findSolution(root, goalTest);
+    Search ASGS = new GraphSearch(new BestFirstFrontier(new AStarFunction(new FurthestCityHeuristicFunction(romania, startCity))));
+    Search ASTS = new TreeSearch(new BestFirstFrontier(new AStarFunction(new FurthestCityHeuristicFunction(romania, startCity))));
+    Node solution = ASTS.findSolution(root, goalTest);
     printing.printSolution(solution);
-    System.out.println("The maximum number of nodes stored in BFTS is " + BFTS.maxNumberOfNodes());
-    System.out.println("The total number of nodes generated in BFTS is " + BFTS.nodesGenerated());
+    System.out.println("The maximum number of nodes stored in ASTS is " + ASTS.maxNumberOfNodes());
+    System.out.println("The total number of nodes generated in ASTS is " + ASTS.nodesGenerated());
     System.out.println("");
-    solution = BFGS.findSolution(root, goalTest);
+    solution = ASGS.findSolution(root, goalTest);
     printing.printSolution(solution);
-    System.out.println("The maximum number of nodes stored in BFGS is " + BFGS.maxNumberOfNodes());
-    System.out.println("The total number of nodes generated in BFGS is " + BFGS.nodesGenerated());
+    System.out.println("The maximum number of nodes stored in ASGS is " + ASGS.maxNumberOfNodes());
+    System.out.println("The total number of nodes generated in ASGS is " + ASGS.nodesGenerated());
     System.out.println("");
-    solution = IDTS.findSolution(root, goalTest);
-    printing.printSolution(solution);
-    System.out.println("The maximum number of nodes stored in IDTS is " + IDTS.maxNumberOfNodes());
-    System.out.println("The total number of nodes generated in IDTS is " + IDTS.nodesGenerated());
-    System.out.println("");
-    solution = DFGS.findSolution(root, goalTest);
-    printing.printSolution(solution);
-    System.out.println("The maximum number of nodes stored in DFGS is " + DFGS.maxNumberOfNodes());
-    System.out.println("The total number of nodes generated in DFGS is " + DFGS.nodesGenerated());
-    System.out.println("");
-    solution = DFTS.findSolution(root, goalTest);
-    printing.printSolution(solution);
-    System.out.println("The maximum number of nodes stored in DFTS is " + DFTS.maxNumberOfNodes());
-    System.out.println("The total number of nodes generated in DFTS is " + DFTS.nodesGenerated());
 	}
 }
--- src/tour/FurthestCityHeuristicFunction.java
+++ src/tour/FurthestCityHeuristicFunction.java
@@ -5,12 +5,30 @@ import java.util.Set;
 import search.Node;
 
 public class FurthestCityHeuristicFunction implements NodeFunction {
+  private Cities cities;
+  private City goalCity;
+  public FurthestCityHeuristicFunction(Cities cities, City goalCity) {
+    this.cities = cities;
+    this.goalCity = goalCity;
+  }
   public int evaluate(Node node) {
     TourState state = (TourState)node.state;
-    return 0;
+    City furthest = findFurthestCity(state.currentCity, state.visitedCities);
+    return state.currentCity.getShortestDistanceTo(furthest) + furthest.getShortestDistanceTo(goalCity);
   }
-  
-  /*private City findFurthestCity(City currentCity, Set<City> visitedCities) {
-    
-  }*/
+  private City findFurthestCity(City currentCity, Set<City> visitedCities) {
+    Set<City> allCities = cities.getAllCities();
+    for (City city: visitedCities) {
+      allCities.remove(city);
+    }
+    City furthest = currentCity;
+    int shortestDist = 0;
+    for (City city: allCities) {
+      if(currentCity.getShortestDistanceTo(city) > shortestDist) {
+        furthest = city;
+        shortestDist = currentCity.getShortestDistanceTo(city);
+      }
+    }
+    return furthest;
+  }
 }
